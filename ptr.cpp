4e22cba796ba7856b5121aa9a8b46b671a3c7ad4
Deadlegend1321 <51445048+Deadlegend1321@users.noreply.github.com>
2019-06-28 00:01:39 +0530

    Update ptr.cpp

--- ptr.cpp
+++ ptr.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main() 
 {
-	int n=5;
-	for(int i=1;i<=n;i++)
-	{int k=n+1,m=k;
-		for(int j=1;j<=n;j++)
+	int n=5,k=n+1,m=k;
+	for(int i=1;i<=2*n-1;i++)
+	{
+		for(int j=1;j<=2*n-1;j++)
 		{
+			if(i<=n && j<=n)
+			{
 				if(j<i)
 				{
 					m=m-1;
@@ -19,6 +21,54 @@ int main()
 				}
 				else
 					cout<<m<<" ";
+			}
+			else if(i<=n && j>n)
+			{
+				if(i+j==2*n)
+				{
+					cout<<m<<" ";
+					m=m+1;
+				}
+				else if(i+j>2*n)
+				{
+					if(m==1)
+						m=m+1;
+					cout<<m<<" ";
+					m=m+1;
+				}
+				else
+					cout<<m<<" ";
+			}
+			else if(i>n && j<=n)
+			{
+				if(i+j==2*n)
+				{
+					m=m-1;
+					cout<<m<<" ";
+				}
+				else if(i+j<2*n)
+				{
+					m=m-1;
+					cout<<m<<" ";
+				}
+				else
+					cout<<m<<" ";
+			}
+			else
+			{
+				if(i==j)
+				{
+					cout<<m<<" ";
+					m=m+1;
+				}
+				else if(i<j)
+				{
+					cout<<m<<" ";
+					m=m+1;
+				}
+				else
+					cout<<m<<" ";
+			}
 		}
 		cout<<endl;
 	}
